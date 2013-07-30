44646d827eb2df6fc8d322f012ec4e7b2b677615
vinitkp <vinit.kp@live.in>
2013-07-30 20:00:15 +0530

    sql file added

--- README.md
+++ README.md
@@ -21,3 +21,19 @@ References
 
 [1] http://topcoder.com
 [2] http://codeforces.com
+
+Status
+=======
+
+-the normal spoj/codeforces style is working fine
+-admin could add + edit the questions
+-topcoder style still needs some work
+-frontend of few pages still needs to be done
+
+Installation
+=============
+
+-create a new database in mysql
+-update the database name, host, username and password of mysql in server_constraints.php
+-run it on your machine and add issues 
+
---
+++ dodge.sql
@@ -0,0 +1,107 @@
+-- phpMyAdmin SQL Dump
+-- version 3.4.10.1deb1
+-- http://www.phpmyadmin.net
+--
+-- Host: localhost
+-- Generation Time: Jul 30, 2013 at 07:52 PM
+-- Server version: 5.5.32
+-- PHP Version: 5.3.10-1ubuntu3.7
+
+SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+
+--
+-- Database: `dodge`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `code_number`
+--
+
+CREATE TABLE IF NOT EXISTS `code_number` (
+  `id` int(11) NOT NULL DEFAULT '1',
+  `number` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `code_number`
+--
+
+INSERT INTO `code_number` (`id`, `number`) VALUES
+(1, 1);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `code_submissions`
+--
+
+CREATE TABLE IF NOT EXISTS `code_submissions` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `username` varchar(255) NOT NULL,
+  `sub_time` datetime NOT NULL,
+  `status` enum('0','1') NOT NULL DEFAULT '1',
+  `question` text NOT NULL,
+  `code_num` int(11) NOT NULL,
+  `result` enum('runtime error','accepted','compilation error','wrong answer') NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=65 ;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `questions`
+--
+
+CREATE TABLE IF NOT EXISTS `questions` (
+  `name` text NOT NULL,
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `difficulty` enum('A','B','C') NOT NULL DEFAULT 'A',
+  `statement` text NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=22 ;
+
+--
+-- Dumping data for table `questions`
+--
+
+INSERT INTO `questions` (`name`, `id`, `difficulty`, `statement`) VALUES
+('square', 1, 'A', 'Find the square of given number.\r\n\r\nInput: first input would be an integer t(<1000) followed by t lines.\r\n    Every line will contain one integer(<100000)\r\n    \r\nOutput: output should contain t lines with each line giving the square of corresponding input.\r\n\r\neg:\r\nInput:\r\n5\r\n1\r\n2\r\n3\r\n4\r\n5\r\n\r\nOutput:\r\n1\r\n4\r\n9\r\n16\r\n25\r\n'),
+('powers', 2, 'A', ''),
+('cube', 18, 'A', 'fdsa'),
+('asd', 21, 'A', 'Question Statement');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE IF NOT EXISTS `users` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `username` varchar(255) NOT NULL,
+  `passwd` varchar(255) NOT NULL,
+  `email` varchar(255) DEFAULT NULL,
+  `college` text,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=19 ;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`id`, `username`, `passwd`, `email`, `college`) VALUES
+(1, 'vinit', 'v', 'vinit', 'daiict'),
+(17, 'admin', 'don', NULL, NULL);
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
